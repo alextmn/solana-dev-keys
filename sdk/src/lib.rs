595a7b723d3b66361f9999aa5af917e37c4f4fa4
alex <alextmn1@gmail.com>
2022-02-11 00:06:57 -0500

    inital commit

---
+++ sdk/src/lib.rs
@@ -0,0 +1,101 @@
+#![allow(incomplete_features)]
+#![cfg_attr(RUSTC_WITH_SPECIALIZATION, feature(specialization))]
+#![cfg_attr(RUSTC_NEEDS_PROC_MACRO_HYGIENE, feature(proc_macro_hygiene))]
+
+// Allows macro expansion of `use ::solana_sdk::*` to work within this crate
+extern crate self as solana_sdk;
+
+#[cfg(feature = "full")]
+pub use signer::signers;
+pub use solana_program::*;
+
+
+pub mod pubkey;
+pub mod signature;
+pub mod signer;
+
+/// Same as `declare_id` except report that this id has been deprecated
+pub use solana_sdk_macro::declare_deprecated_id;
+/// Convenience macro to declare a static public key and functions to interact with it
+///
+/// Input: a single literal base58 string representation of a program's id
+///
+/// # Example
+///
+/// ```
+/// # // wrapper is used so that the macro invocation occurs in the item position
+/// # // rather than in the statement position which isn't allowed.
+/// use std::str::FromStr;
+/// use solana_sdk::{declare_id, pubkey::Pubkey};
+///
+/// # mod item_wrapper {
+/// #   use solana_sdk::declare_id;
+/// declare_id!("My11111111111111111111111111111111111111111");
+/// # }
+/// # use item_wrapper::id;
+///
+/// let my_id = Pubkey::from_str("My11111111111111111111111111111111111111111").unwrap();
+/// assert_eq!(id(), my_id);
+/// ```
+pub use solana_sdk_macro::declare_id;
+/// Convenience macro to define a static public key
+///
+/// Input: a single literal base58 string representation of a Pubkey
+///
+/// # Example
+///
+/// ```
+/// use std::str::FromStr;
+/// use solana_program::{pubkey, pubkey::Pubkey};
+///
+/// static ID: Pubkey = pubkey!("My11111111111111111111111111111111111111111");
+///
+/// let my_id = Pubkey::from_str("My11111111111111111111111111111111111111111").unwrap();
+/// assert_eq!(ID, my_id);
+/// ```
+pub use solana_sdk_macro::pubkey;
+pub use solana_sdk_macro::pubkeys;
+#[rustversion::since(1.46.0)]
+pub use solana_sdk_macro::respan;
+
+// Unused `solana_sdk::program_stubs!()` macro retained for source backwards compatibility with older programs
+#[macro_export]
+#[deprecated(
+    since = "1.4.3",
+    note = "program_stubs macro is obsolete and can be safely removed"
+)]
+macro_rules! program_stubs {
+    () => {};
+}
+
+/// Convenience macro for `AddAssign` with saturating arithmetic.
+/// Replace by `std::num::Saturating` once stable
+#[macro_export]
+macro_rules! saturating_add_assign {
+    ($i:expr, $v:expr) => {{
+        $i = $i.saturating_add($v)
+    }};
+}
+
+#[macro_use]
+extern crate serde_derive;
+pub extern crate bs58;
+extern crate log as logger;
+
+#[macro_use]
+extern crate solana_frozen_abi_macro;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_saturating_add_assign() {
+        let mut i = 0u64;
+        let v = 1;
+        saturating_add_assign!(i, v);
+        assert_eq!(i, 1);
+
+        i = u64::MAX;
+        saturating_add_assign!(i, v);
+        assert_eq!(i, u64::MAX);
+    }
+}
---
+++ sdk/src/signer/presigner.rs
@@ -0,0 +1,62 @@
+#![cfg(feature = "full")]
+
+use {
+    crate::{
+        pubkey::Pubkey,
+        signature::Signature,
+        signer::{Signer, SignerError},
+    },
+    thiserror::Error,
+};
+
+/// A `Signer` implementation that represents a `Signature` that has been
+/// constructed externally. Performs a signature verification against the
+/// expected message upon `sign()` requests to affirm its relationship to
+/// the `message` bytes
+#[derive(Clone, Debug, Default)]
+pub struct Presigner {
+    pubkey: Pubkey,
+    signature: Signature,
+}
+
+impl Presigner {
+    pub fn new(pubkey: &Pubkey, signature: &Signature) -> Self {
+        Self {
+            pubkey: *pubkey,
+            signature: *signature,
+        }
+    }
+}
+
+#[derive(Debug, Error, PartialEq)]
+pub enum PresignerError {
+    #[error("pre-generated signature cannot verify data")]
+    VerificationFailure,
+}
+
+impl Signer for Presigner {
+    fn try_pubkey(&self) -> Result<Pubkey, SignerError> {
+        Ok(self.pubkey)
+    }
+
+    fn try_sign_message(&self, message: &[u8]) -> Result<Signature, SignerError> {
+        if self.signature.verify(self.pubkey.as_ref(), message) {
+            Ok(self.signature)
+        } else {
+            Err(PresignerError::VerificationFailure.into())
+        }
+    }
+
+    fn is_interactive(&self) -> bool {
+        false
+    }
+}
+
+impl<T> PartialEq<T> for Presigner
+where
+    T: Signer,
+{
+    fn eq(&self, other: &T) -> bool {
+        self.pubkey() == other.pubkey()
+    }
+}
